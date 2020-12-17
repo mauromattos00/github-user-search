2d3f60b7b501d657bcb1af59535fd565480d7a67
Mauro Mattos <mauromattos00@gmail.com>
2020-12-17 01:34:47 -0300

    fix(eslint): fix issues

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -2,18 +2,18 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter, StacktraceOption } = require('jasmine-spec-reporter');
+import { SpecReporter, StacktraceOption } from 'jasmine-spec-reporter';
+import tsnode from 'ts-node';
+import path from 'path';
 
 /**
  * @type { import("protractor").Config }
  */
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './src/**/*.e2e-spec.ts'
-  ],
+  specs: ['./src/**/*.e2e-spec.ts'],
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'chrome',
   },
   directConnect: true,
   SELENIUM_PROMISE_MANAGER: false,
@@ -22,16 +22,19 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    print: function () {},
   },
   onPrepare() {
-    require('ts-node').register({
-      project: require('path').join(__dirname, './tsconfig.json')
+    tsnode.register({
+      project: path.join(__dirname, './tsconfig.json'),
     });
-    jasmine.getEnv().addReporter(new SpecReporter({
-      spec: {
-        displayStacktrace: StacktraceOption.PRETTY
-      }
-    }));
-  }
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        spec: {
+          displayStacktrace: StacktraceOption.PRETTY,
+        },
+      }),
+    );
+  },
 };
