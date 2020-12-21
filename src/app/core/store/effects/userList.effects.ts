0ffe9703b476c5116a917fa6035ad2c1ffa480f4
Mauro Mattos <mauromattos00@gmail.com>
2020-12-20 23:13:00 -0300

    feat(user-list): implements ngrx effects

---
+++ src/app/core/store/effects/userList.effects.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { EMPTY } from 'rxjs';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { UserListService } from '../../services/userList.service';
+
+import { IUser } from '../models/user.model';
+
+import { search, searchSuccess } from '../actions/userList.actions';
+
+@Injectable()
+export class UserListEffects {
+  fetchUsers$ = createEffect((): any =>
+    this.actions$.pipe(
+      ofType(search),
+      mergeMap((action) =>
+        this.userListService.searchUsers(action.query).pipe(
+          map((users: any) => (users.items.length > 0 ? users.items : [])),
+          map((users: IUser[]) => {
+            if (users.length > 0) {
+              return users.map((user: any) => ({
+                username: user.login,
+                avatar: user.avatar_url,
+                url: user.html_url,
+                apiUrl: user.url,
+              }));
+            }
+            return [];
+          }),
+          map((users: IUser[]) => ({
+            type: '[USER] Search Success',
+            props: users,
+          })),
+          catchError(() => EMPTY),
+        ),
+      ),
+    ),
+  );
+
+  constructor(
+    private actions$: Actions,
+    private userListService: UserListService,
+  ) {}
+}
