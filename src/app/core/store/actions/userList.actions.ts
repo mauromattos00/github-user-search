54ef87cb4e9f19efbe5b34754d88c65624d6cc67
Mauro Mattos <mauromattos00@gmail.com>
2020-12-20 23:09:44 -0300

    feat(user-list): implements ngrx actions

--- src/app/core/store/actions/userList.actions.ts
+++ src/app/core/store/actions/userList.actions.ts
@@ -1,3 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 
 export const search = createAction('[USER] Search', props<{ query: string }>());
+export const searchSuccess = createAction(
+  '[USER] Search Success',
+  props<any>(),
+);
