fb4a1207a71daae4e3327a0c07a15551b1f235d3
Mauro Mattos <mauromattos00@gmail.com>
2020-12-20 23:21:17 -0300

    refactor: removes logic from app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AtomsModule } from './components/atoms/atoms.module';
 import { MoleculesModule } from './components/molecules/molecules.module';
+import { OrganismsModule } from './components/organisms/organisms.module';
 
 import { userListReducer } from './core/store/reducers/userList.reducer';
+
+import { UserListEffects } from './core/store/effects/userList.effects';
 
 import { AppComponent } from './app.component';
 
@@ -14,12 +19,15 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AtomsModule,
     MoleculesModule,
+    OrganismsModule,
     StoreModule.forRoot({ userList: userListReducer }),
     StoreDevtoolsModule.instrument({
       maxAge: 20,
     }),
+    EffectsModule.forRoot([UserListEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
