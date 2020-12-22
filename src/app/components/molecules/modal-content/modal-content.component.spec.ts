46c7a34437318c7c9e37e5f1f0286d81572a0284
Mauro Mattos <mauromattos00@gmail.com>
2020-12-21 22:24:40 -0300

    test(modal-content): implements test

--- src/app/components/molecules/modal-content/modal-content.component.spec.ts
+++ src/app/components/molecules/modal-content/modal-content.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ModalContentComponent } from './modal-content.component';
 
@@ -9,16 +15,38 @@ describe('ModalContentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ModalContentComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [MatDialogModule],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+      ],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ModalContentComponent);
     component = fixture.componentInstance;
+    component.data = {
+      user: {
+        username: 'mauromattos00',
+        htmlUrl: 'https://github.com/mauromattos00',
+        avatar: '#',
+        url: '#',
+      },
+    };
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should render component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should consume user GitHub information', () => {
+    const element: HTMLElement = fixture.nativeElement;
+    expect(
+      element.querySelector('.modal-content__text').textContent.split(' '),
+    ).toContain(component.data.user.username);
+    expect(element.querySelector('a').href).toBe(component.data.user.htmlUrl);
   });
 });
