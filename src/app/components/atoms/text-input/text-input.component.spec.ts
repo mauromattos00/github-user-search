7b8520c4590b5c8074ea277ef15a55ffc18c69b7
Mauro Mattos <mauromattos00@gmail.com>
2020-12-22 15:34:52 -0300

    test(text-input): implements tests

--- src/app/components/atoms/text-input/text-input.component.spec.ts
+++ src/app/components/atoms/text-input/text-input.component.spec.ts
@@ -1,24 +1,41 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TextInputComponent } from './text-input.component';
+import { By } from '@angular/platform-browser';
 
 describe('TextInputComponent', () => {
   let component: TextInputComponent;
   let fixture: ComponentFixture<TextInputComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [TextInputComponent],
+      imports: [CommonModule, FormsModule],
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TextInputComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should receive placeholder as input', () => {
+    component.placeholder = 'placeholder text';
+    fixture.detectChanges();
+
+    expect(
+      debugElement.query(By.css('.search-input')).nativeElement.placeholder,
+    ).toBe('placeholder text');
   });
 });
