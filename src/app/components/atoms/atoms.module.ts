cc6024a08840e7b51a8c2b601fb9b23de3b3ba66
Mauro Mattos <mauromattos00@gmail.com>
2020-12-22 17:30:11 -0300

    feat(spinner): implements spinner component

--- src/app/components/atoms/atoms.module.ts
+++ src/app/components/atoms/atoms.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { ButtonComponent } from './button/button.component';
 import { TextInputComponent } from './text-input/text-input.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  exports: [ButtonComponent, TextInputComponent],
-  declarations: [ButtonComponent, TextInputComponent],
+  exports: [ButtonComponent, TextInputComponent, SpinnerComponent],
+  declarations: [ButtonComponent, TextInputComponent, SpinnerComponent],
   providers: [],
 })
 export class AtomsModule {}
