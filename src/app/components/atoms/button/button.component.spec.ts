0b8f7c989c71c613e00b65c57d57cd84f6daa46b
Mauro Mattos <mauromattos00@gmail.com>
2020-12-21 21:27:16 -0300

    test(button): implements tests

--- src/app/components/atoms/button/button.component.spec.ts
+++ src/app/components/atoms/button/button.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { isRegExp } from 'util';
 
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  let buttonElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [ButtonComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    buttonElement = fixture.nativeElement.querySelector('button');
   });
 
-  it('should create', () => {
+  it('Should render button', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should render primary button', () => {
+    component.type = 'primary';
+    fixture.detectChanges();
+    expect(buttonElement.classList).toContain('btn--primary');
+  });
+
+  it('Should render ghost button', () => {
+    component.type = 'ghost';
+    fixture.detectChanges();
+    expect(buttonElement.classList).toContain('btn--ghost');
   });
 });
