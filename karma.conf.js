533cf0bc55638c1c7adda8c5de3f684d9b428108
Mauro Mattos <mauromattos00@gmail.com>
2020-12-22 16:49:21 -0300

    test: inserts chrome as test browser

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-firefox-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma'),
@@ -38,7 +39,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
+    browsers: ['Firefox', 'Chrome'],
     singleRun: false,
     restartOnFileChange: true,
   });
